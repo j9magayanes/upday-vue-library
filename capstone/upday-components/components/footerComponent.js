0bf1287d31863e7e3c5044d4d81491e763817aed
janine <janine@upday.com>
2023-11-08 19:55:37 +0100

    fix: footer comp

--- capstone/upday-components/components/footerComponent.js
+++ capstone/upday-components/components/footerComponent.js
@@ -1,44 +1,39 @@
 class FooterComponent extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
     
-        this.attachShadow({ mode: 'open' });
-    
-        this.shadowRoot.innerHTML = `
-        <footer className='padded-lr'>
+    this.shadowRoot.innerHTML = `
+      <footer class='padded-lr'>
         <ul>
-        <>
-            <li>
-             <a>
-                <slot></slot>
-              </a>
-            </li>
-            </>
+          <li>
+            <a>
+              <slot></slot>
+            </a>
+          </li>
         </ul>
         <p>Copyright 2023, upday GmbH & Co. KG</p>
       </footer>
-                  `;
-        this.textContent = this.shadowRoot.querySelector('slot');
-        this.link = this.shadowRoot.querySelector('a');
-      }
-        
-    
-      static get observedAttributes() {
-        return [ 'text','class-prop', 'callback', 'href'];
-      }
-    
-      attributeChangedCallback(name, oldValue, newValue) {
-        if (name === 'class-prop') {
-            this.contentElement.class = newValue;
-        } else if (name === 'text') {
-          this.textContent.setAttribute('text', newValue);
-        } else if (name === 'callback') {
-            this.link.setAttribute('callback', newValue);
-          } else if (name === 'href'){
-            this.link.setAttribute('href', newValue);
-          }
-      }
+    `;
+
+    this.link = this.shadowRoot.querySelector('a');
+  }
+
+  static get observedAttributes() {
+    return ['class-prop', 'text', 'callback', 'href'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'class-prop') {
+      this.link.setAttribute('class', newValue);
+    } else if (name === 'text') {
+      this.link.textContent = newValue;
+    } else if (name === 'callback') {
+      // Handle callback attribute change if needed
+    } else if (name === 'href') {
+      this.link.setAttribute('href', newValue);
     }
-  
-  customElements.define('footer-component', FooterComponent);
-  
+  }
+}
+
+customElements.define('footer-component', FooterComponent);
