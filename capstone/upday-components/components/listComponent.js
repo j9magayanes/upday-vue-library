dc5f72285d5179735e2a64d47cf467f6064905f0
janine <janine@upday.com>
2023-11-09 20:15:37 +0100

    fix: removed default var

--- capstone/upday-components/components/listComponent.js
+++ capstone/upday-components/components/listComponent.js
@@ -1,53 +1,37 @@
 class ListComponent extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-  
-  
- // Default items array
- this.defaultItems = [
-  { name: 'Default Item 1', href: 'https://www.google.com/' },
-  { name: 'Default Item 2', href: 'https://www.google.com/' },
-  // Add more default items as needed
-];
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
 
-// Set default items if 'items' attribute is not provided
-if (!this.hasAttribute('items')) {
-  this.setAttribute('items', JSON.stringify(this.defaultItems));
-}
-}
-    attributeChangedCallback(name, oldValue, newValue) {
-      console.log(`Attribute "${name}" changed. New value:`, newValue);
-      if (name === 'items') {
-        try {
-          const parsedItems = JSON.parse(newValue);
-          this.renderList(parsedItems);
-        } catch (error) {
-          console.error('Error parsing JSON:', error);
-        }
+  static get observedAttributes() {
+    return ['items'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'items') {
+      try {
+        const parsedItems = JSON.parse(newValue);
+        this.renderList(parsedItems);
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
       }
     }
-  
-    renderList(items) {
-      this.shadowRoot.innerHTML = `
-      <style>
-      /* Add your CSS styles here */
-    </style>
-    <ul>
-      ${items.map((item, index) => `<li data-index="${index}"><a href="${item.href}">${item.name}</a></li>`).join('')}
-    </ul>
-    <p>Copyright 2023, upday GmbH & Co. KG</p>
-      `;
-  
-      const listItems = this.shadowRoot.querySelectorAll('li');
-      listItems.forEach((item) => {
-        item.addEventListener('click', () => {
-          const index = item.getAttribute('data-index');
-          this.dispatchEvent(new CustomEvent('item-clicked', { detail: { index, value: items[index] } }));
-        });
-      });
-    }
   }
-  
-  customElements.define('list-component', ListComponent);
-  
+
+  renderList(items) {
+    this.shadowRoot.innerHTML = `
+      <style>
+        /* Add your CSS styles here */
+      </style>
+      <ul>
+        ${items.map((item, index) => `<li data-index="${index}"><a href="${item.href}">${item.name}</a></li>`).join('')}
+      </ul>
+      <p>Copyright 2023, upday GmbH & Co. KG</p>
+    `;
+
+
+  }
+}
+
+customElements.define('list-component', ListComponent);
