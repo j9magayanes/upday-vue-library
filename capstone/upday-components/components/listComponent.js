ae19c2cf282ee56bc81a853239d5be8ae21bc0e7
janine <janine@upday.com>
2023-11-09 19:53:09 +0100

    fix: added default items to listcomp

--- capstone/upday-components/components/listComponent.js
+++ capstone/upday-components/components/listComponent.js
@@ -2,15 +2,29 @@ class ListComponent extends HTMLElement {
     constructor() {
       super();
       this.attachShadow({ mode: 'open' });
-    }
   
-    static get observedAttributes() {
-      return ['items'];
-    }
   
+ // Default items array
+ this.defaultItems = [
+  { name: 'Default Item 1', href: 'https://www.google.com/' },
+  { name: 'Default Item 2', href: 'https://www.google.com/' },
+  // Add more default items as needed
+];
+
+// Set default items if 'items' attribute is not provided
+if (!this.hasAttribute('items')) {
+  this.setAttribute('items', JSON.stringify(this.defaultItems));
+}
+}
     attributeChangedCallback(name, oldValue, newValue) {
+      console.log(`Attribute "${name}" changed. New value:`, newValue);
       if (name === 'items') {
-        this.renderList(JSON.parse(newValue));
+        try {
+          const parsedItems = JSON.parse(newValue);
+          this.renderList(parsedItems);
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+        }
       }
     }
   
@@ -20,7 +34,7 @@ class ListComponent extends HTMLElement {
       /* Add your CSS styles here */
     </style>
     <ul>
-      ${items.map((item, index) => `<li data-index="${index}"><a href=${item.href}>${item.name}</a></li>`).join('')}
+      ${items.map((item, index) => `<li data-index="${index}"><a href="${item.href}">${item.name}</a></li>`).join('')}
     </ul>
     <p>Copyright 2023, upday GmbH & Co. KG</p>
       `;
