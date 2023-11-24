f8478dba5bbcfdb14ce8474431c71bfc52f60bc9
janine <janine@upday.com>
2023-11-24 19:50:50 +0100

    delete

--- capstone/upday-vue-library/src/App.vue
+++ capstone/upday-vue-library/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-import Ad from './components/Ad/Ad.vue'
 </script>
 
 <template>
